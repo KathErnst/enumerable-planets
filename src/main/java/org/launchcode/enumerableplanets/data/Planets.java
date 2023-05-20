310c5322e5dd22810be5c304b4f2012b79eeff66
katherineernsting <katherine.e.kennison@gmail.com>
2023-05-19 22:27:50 -0500

    Added planet data and table via enum

---
+++ src/main/java/org/launchcode/enumerableplanets/data/Planets.java
@@ -0,0 +1,33 @@
+package org.launchcode.enumerableplanets.data;
+
+public enum Planets {
+    MERCURY("Mercury", 88, 0),
+    VENUS("Venus", 225, 0),
+    EARTH("Earth", 365, 1),
+    MARS("Mars", 687, 2),
+    JUPITER("Jupiter", 4333, 95),
+    SATURN("Saturn", 10759, 124),
+    URANUS("Uranus", 30687, 27),
+    NEPTUNE("Neptune", 60200, 14);
+    private final String name;
+    private final int yearLength;
+    private final int numMoons;
+
+    Planets(String name, int yearLength, int numMoons) {
+        this.name = name;
+        this.yearLength = yearLength;
+        this.numMoons = numMoons;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getYearLength() {
+        return yearLength;
+    }
+
+    public int getNumMoons() {
+        return numMoons;
+    }
+}
